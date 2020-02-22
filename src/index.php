87ba8a185afcd6ac63c021d52952be0334f2a58a
DanielleInkster <danielle.inkster@gmail.com>
2020-02-22 18:27:33 +0000

    Basic form - test

--- src/index.php
+++ src/index.php
@@ -2,7 +2,12 @@
 <htmL>
     <body>
         <?php
-        echo "Hello world";
+        echo "<h1>Register</h1>";
+        echo "<form method='get'>";
+        echo "First Name: <input type='text' name='name'><br>";
+        echo "Surname: <input type='text' name='email'><br>";
+        echo "<input type='submit'>";
+        echo "</form>";
         ?>
     </body>
 </htmL>
