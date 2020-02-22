2ae619d5b7142d15ec3db87c0d7764d62d646b22
DanielleInkster <danielle.inkster@gmail.com>
2020-02-22 18:44:29 +0000

    Runs a second file on submit

--- src/index.php
+++ src/index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
+<?php include "welcome.php";?>
 <htmL>
     <body>
         <?php
         echo "<h1>Register</h1>";
-        echo "<form method='get'>";
+        echo "<form method='post' action ='welcome.php'>";
         echo "First Name: <input type='text' name='name'><br>";
-        echo "Surname: <input type='text' name='email'><br>";
+        echo "Surname: <input type='text' name='surname'><br>";
         echo "<input type='submit'>";
         echo "</form>";
         ?>
