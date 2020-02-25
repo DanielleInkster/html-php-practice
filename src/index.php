60b798163ff5d32b00d260b71ff122958b110455
DanielleInkster <danielle.inkster@gmail.com>
2020-02-25 00:12:44 +0000

    Submit username

--- src/index.php
+++ src/index.php
@@ -6,7 +6,8 @@
         echo "<h1>Register</h1>";
         echo "<form method='post' action ='welcome.php'>";
         echo "First Name: <input type='text' name='name'><br>";
-        echo "Surname: <input type='text' name='email'><br>";
+        echo "Surname: <input type='text' name='surname'><br>";
+        echo "Username: <input type='text' name='username'><br>";
         echo "<input type='submit' name='submit'>";
         echo "</form>";
         ?>
--- src/welcome.php
+++ src/welcome.php
@@ -1,4 +1,5 @@
 <?php 
     if(isset($_POST["submit"])){
         echo "Welcome {$_POST['name']}";
+        echo "\nYour new username is {$_POST['username']}";
     }; 
